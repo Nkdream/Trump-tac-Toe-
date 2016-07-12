0bb45a30dfd3e3a8251ab9fa5955eb41276e6d9d
Nick L <Nick@nicks-macbook-pro-2.local>
2016-07-12 11:30:18 -0700

    Initial Commit

---
+++ Real tic tacUITests/Real_tic_tacUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Real_tic_tacUITests.swift
+//  Real tic tacUITests
+//
+//  Created by Nick L on 7/12/16.
+//  Copyright © 2016 Nick L. All rights reserved.
+//
+
+import XCTest
+
+class Real_tic_tacUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
